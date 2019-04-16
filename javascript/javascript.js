a87116c15a3c1fe05da5139e98e6687aa3b9050e
Cameron Finley <cjfinley@ncsu.edu>
2019-04-15 22:05:19 -0400

    More of the basic background done

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,26 +4,61 @@ const humidityVal = 0.5;
 const lightVal = 0.5;
 const movementVal = 0.5;
 
-// References the Octave value
-var octaveSlider = document.getElementById("octaveSlider");
-var octaveOutput = document.getElementById("octaveValue");
+var loopLocation = 0;
+
+// References the Overall Volume value
+var volumeSlider = document.getElementById("volumeSlider");
+var volumeOutput = document.getElementById("volumeValue");
+// References the Temperature Volume value
+var tempVolumeSlider = document.getElementById("tempVolumeSlider");
+var tempVolumeOutput = document.getElementById("tempVolumeValue");
+// References the Light Volume value
+var lightVolumeSlider = document.getElementById("lightVolumeSlider");
+var lightVolumeOutput = document.getElementById("lightVolumeValue");
+// References the Humidity Volume value
+var humidityVolumeSlider = document.getElementById("humidityVolumeSlider");
+var humidityVolumeOutput = document.getElementById("humidityVolumeValue");
+// References the Motion Volume value
+var motionVolumeSlider = document.getElementById("motionVolumeSlider");
+var motionVolumeOutput = document.getElementById("motionVolumeValue");
 
 // Some declarations of variables
-// This is the loop used for the undertone white noise
-let rhythm;
 // This is the synth used currently, we'll add more here for each input
-let tempSynth;
-// bass synth
-let bassSynth;
+let humiditySynth;
+// This is the loop used for the undertone white noise
+let humidityLoop;
 
-// Whether or not there should be any rhythm
-let hasRhythm;
+// bass synth
+let lightSynth;
+// This is a function called by Tone.Sequence that simply plays a series of notes
+var lightLoop;
+
+// synth for temperature
+let tempSynth;
+// This loop simply plays chords every whole note
+var tempLoop;
+
+// Loop to keep track of which measure it is on
+var timeLoop;
+
+// Loop for making things funky
+let makeFunkyLoop;
+// Make funky value changes the ocatave at random
+var makeFunkyVal = false;
+
 // A boolean variable used to tell us when to run the setup function so it is only run once
 var hasStarted = false;
-// This is a function called by Tone.Sequence that simply plays a series of notes
-var baseLine;
-//
-baseLineNotes = [
+
+//	PIANO
+var piano = new Tone.PolySynth(4, Tone.Synth, {
+  volume: -8,
+  oscillator: {
+    partials: [1, 2, 5]
+  },
+  portamento: 0.005
+}).toMaster();
+
+lightLoopNotes = [
   // A minor chord
   "a" + 1,
   "c" + 2,
@@ -62,93 +97,161 @@ baseLineNotes = [
   ["b" + 1, "c" + 2]
 ];
 
+var aChord = ["a3", "c4", "e4"];
+var fChord = ["a3", "c4", "f4"];
+var cChord = ["c4", "e4", "g4"];
+var gChord = ["g3", "b3", "d4"];
+// These are tone.js part objects for each chord initialized in the setup function
+var aChordPart;
+var fChordPart;
+var cChordPart;
+var gChordPart;
+
 // Setupt
 function setup() {
   // Setup synth
-  tempSynth = new Tone.MembraneSynth();
+  humiditySynth = new Tone.MembraneSynth().toMaster();
+
+  lightSynth = new Tone.Synth().toMaster();
+
+  tempSynth = new Tone.PolySynth(6);
+  tempSynth.volume.value = -10;
   tempSynth.toMaster();
-  bassSynth = new Tone.Synth();
-  bassSynth.toMaster();
 
-  tempFrequency = 100;
+  lightLoop = new Tone.Sequence(
+    lightLoopFunction,
+    lightLoopNotes,
+    "8n"
+  ).start();
+  humidityLoop = new Tone.Loop(song, "4n").start();
+  tempLoop = new Tone.Loop(tempLoopFunction, "1n").start();
+  makeFunkyLoop = new Tone.Loop(makeFunkySong, "8n").start();
 
-  // initializes the hasRhythm variable to the value of the checkbox
-  hasRhythm = true;
-  // the startOctave
-  octave = 0;
+  timeLoop = new Tone.Loop(timeLoopFunction, "1n").start();
 
-  baseLine = new Tone.Sequence(baseLine, baseLineNotes, "8n").start();
-  // Initialize the loop
-  rhythm = new Tone.Loop(song, "4n").start();
+  setlightLoopOctave(1);
 }
 
-function toggleRhythm() {
-  if (hasRhythm) {
-    hasRhythm = false;
-    rhythm.stop();
+function makeFunky() {
+  if (makeFunkyVal) {
+    makeFunkyVal = false;
+    // ensures that the melody is still playing
+    lightLoop.mute = false;
+    setlightLoopOctave(2);
   } else {
-    hasRhythm = true;
-    rhythm.start(0);
+    makeFunkyVal = true;
   }
 }
 
-function setBaseLineOctave(octave) {
-  newValue = parseInt(octave) - 1;
-  baseLine.at(0, "a" + (1 + newValue));
-  baseLine.at(1, "c" + (2 + newValue));
-  baseLine.at(2, "e" + (2 + newValue));
-  baseLine.at(3, ["d" + (2 + newValue), "c" + (2 + newValue)]);
-  baseLine.at(4, "a" + (1 + newValue));
-  baseLine.at(5, "c" + (2 + newValue));
-  baseLine.at(6, "e" + (2 + newValue));
-  baseLine.at(7, "c" + (2 + newValue));
-  // f major chord
-  baseLine.at(8, "a" + (1 + newValue));
-  baseLine.at(9, "c" + (2 + newValue));
-  baseLine.at(10, "f" + (2 + newValue));
-  baseLine.at(11, ["e" + (2 + newValue), "c" + (2 + newValue)]);
-  baseLine.at(12, "a" + (1 + newValue));
-  baseLine.at(13, "c" + (2 + newValue));
-  baseLine.at(14, "f" + (2 + newValue));
-  baseLine.at(15, null);
-  // c major chord
-  baseLine.at(16, "g" + (1 + newValue));
-  baseLine.at(17, "c" + (2 + newValue));
-  baseLine.at(18, "e" + (2 + newValue));
-  baseLine.at(19, [null, "c" + (2 + newValue)]);
-  baseLine.at(20, "g" + (1 + newValue));
-  baseLine.at(21, "c" + (2 + newValue));
-  baseLine.at(22, "e" + (2 + newValue));
-  baseLine.at(23, "c" + (2 + newValue));
-  // g major chord
-  baseLine.at(24, "g" + (1 + newValue));
-  baseLine.at(25, "b" + (1 + newValue));
-  baseLine.at(26, "d" + (2 + newValue));
-  baseLine.at(27, ["c" + (2 + newValue), "b" + (1 + newValue)]);
-  baseLine.at(28, "c" + (2 + newValue));
-  baseLine.at(29, ["b" + (1 + newValue), "c" + (2 + newValue)]);
-  baseLine.at(30, ["d" + (2 + newValue), "c" + (2 + newValue)]);
-  baseLine.at(31, ["b" + (1 + newValue), "c" + (2 + newValue)]);
+function makeFunkySong(time) {
+  if (makeFunkyVal) {
+    val = Math.random();
+    val *= 5;
+    val = Math.floor(val) + 1;
+    if (val == 5) {
+      lightLoop.mute = true;
+    } else {
+      lightLoop.mute = false;
+      setlightLoopOctave(val);
+    }
+  }
 }
 
-octaveSlider.oninput = function() {
-  octaveOutput.innerHTML = this.value;
-  setBaseLineOctave(this.value);
+function setlightLoopOctave(octave) {
+  newValue = parseInt(octave) - 1;
+  lightLoop.at(0, "a" + (1 + newValue));
+  lightLoop.at(1, "c" + (2 + newValue));
+  lightLoop.at(2, "e" + (2 + newValue));
+  lightLoop.at(3, ["d" + (2 + newValue), "c" + (2 + newValue)]);
+  lightLoop.at(4, "a" + (1 + newValue));
+  lightLoop.at(5, "c" + (2 + newValue));
+  lightLoop.at(6, "e" + (2 + newValue));
+  lightLoop.at(7, "c" + (2 + newValue));
+  // f major chord
+  lightLoop.at(8, "a" + (1 + newValue));
+  lightLoop.at(9, "c" + (2 + newValue));
+  lightLoop.at(10, "f" + (2 + newValue));
+  lightLoop.at(11, ["e" + (2 + newValue), "c" + (2 + newValue)]);
+  lightLoop.at(12, "a" + (1 + newValue));
+  lightLoop.at(13, "c" + (2 + newValue));
+  lightLoop.at(14, "f" + (2 + newValue));
+  lightLoop.at(15, null);
+  // c major chord
+  lightLoop.at(16, "g" + (1 + newValue));
+  lightLoop.at(17, "c" + (2 + newValue));
+  lightLoop.at(18, "e" + (2 + newValue));
+  lightLoop.at(19, [null, "c" + (2 + newValue)]);
+  lightLoop.at(20, "g" + (1 + newValue));
+  lightLoop.at(21, "c" + (2 + newValue));
+  lightLoop.at(22, "e" + (2 + newValue));
+  lightLoop.at(23, "c" + (2 + newValue));
+  // g major chord
+  lightLoop.at(24, "g" + (1 + newValue));
+  lightLoop.at(25, "b" + (1 + newValue));
+  lightLoop.at(26, "d" + (2 + newValue));
+  lightLoop.at(27, ["c" + (2 + newValue), "b" + (1 + newValue)]);
+  lightLoop.at(28, "c" + (2 + newValue));
+  lightLoop.at(29, ["b" + (1 + newValue), "c" + (2 + newValue)]);
+  lightLoop.at(30, ["d" + (2 + newValue), "c" + (2 + newValue)]);
+  lightLoop.at(31, ["b" + (1 + newValue), "c" + (2 + newValue)]);
+}
+
+volumeSlider.oninput = function() {
+  volumeOutput.innerHTML = this.value;
+  // Can have this change whatever value needs to be tested
+  Tone.Master.volume.value = this.value;
 };
 
-// This determines the rhythm
+tempVolumeSlider.oninput = function() {
+  tempVolumeOutput.innerHTML = this.value;
+  // Can have this change whatever value needs to be tested
+  tempSynth.volume.value = this.value - 10;
+};
+
+lightVolumeSlider.oninput = function() {
+  lightVolumeOutput.innerHTML = this.value;
+  // Can have this change whatever value needs to be tested
+  lightSynth.volume.value = this.value;
+};
+
+humidityVolumeSlider.oninput = function() {
+  humidityVolumeOutput.innerHTML = this.value;
+  // Can have this change whatever value needs to be tested
+  humiditySynth.volume.value = this.value;
+};
+
+// This determines the humidityLoop
 function song(time) {
-  tempSynth.triggerAttackRelease("c1", "8n", time);
+  humiditySynth.triggerAttackRelease("c1", "8n", time);
+}
+
+function tempLoopFunction(time) {
+  if (loopLocation == 0) {
+    tempSynth.triggerAttackRelease(aChord, "2n", time);
+  } else if (loopLocation == 1) {
+    tempSynth.triggerAttackRelease(fChord, "2n", time);
+  } else if (loopLocation == 2) {
+    tempSynth.triggerAttackRelease(cChord, "2n", time);
+  } else {
+    tempSynth.triggerAttackRelease(gChord, "2n", time);
+  }
 }
 
 // Music note for the base line
-function baseLine(time, note) {
-  bassSynth.triggerAttackRelease(note, "10hz", time);
+function lightLoopFunction(time, note) {
+  lightSynth.triggerAttackRelease(note, "10hz", time);
+}
+
+function timeLoopFunction(time) {
+  loopLocation += 1;
+  loopLocation = loopLocation % 4;
+  console.log(loopLocation);
 }
 
 // This stops all the audio output
 function pause() {
   Tone.Transport.stop();
+  loopLocation = 0;
 }
 
 // This plays all the audio output
@@ -160,17 +263,61 @@ function play() {
   Tone.Transport.start();
 }
 
-// This adds the sound gotten from the light value
-function light() {
-  // play a sound dependent on the input light value
+// This mutes the Transport
+function mute() {
+  if (Tone.Master.mute) {
+    Tone.Master.mute = false;
+  } else {
+    Tone.Master.mute = true;
+  }
 }
 
-// This adds the sound gotten from the humidity values
-function humidity() {
-  // play a sound dependent on the input humidity
+function toggleTemp() {
+  if (tempLoop.mute) {
+    tempLoop.mute = false;
+  } else {
+    tempLoop.mute = true;
+  }
+}
+
+function toggleLight() {
+  if (lightLoop.mute) {
+    lightLoop.mute = false;
+  } else {
+    lightLoop.mute = true;
+  }
+}
+
+function toggleHumidity() {
+  if (humidityLoop.mute) {
+    humidityLoop.mute = false;
+  } else {
+    humidityLoop.mute = true;
+  }
 }
 
 // This adds the sound gotten from the motion values
 function motion() {
   // play a sound dependent on the motion inputs
+
+  // X axis
+  if (x < 0) {
+    // play positive arpegio
+  } else {
+    // play negative arpegio
+  }
+
+  // y axis
+  if (y < 0) {
+    // play positive arpegio
+  } else {
+    // play negative arpegio
+  }
+
+  // z axis
+  if (z < 0) {
+    // play positive arpegio
+  } else {
+    // play negative arpegio
+  }
 }
